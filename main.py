fe9215c020cb9a1d09fe9e97fdb350d740860944
unknown <ninjapaimen@gmail.com>
2021-11-28 18:37:14 +0200

    Added some comments

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def main(args):
     # check if time range is more than 90 days
     if args.date_to - args.date_from < datetime.timedelta(days=90):
         l = True
+        # The API returns daily data (00:00 UTC) if time range is above 90 days
         extended = args.date_to + datetime.timedelta(days=90)
 
     try:
@@ -22,7 +23,7 @@ def main(args):
     except requests.exceptions.RequestException as ex:
         raise SystemExit(ex)
 
-    try:
+    try:  # Only relevant values from the data
         if l:
             end = (extended-args.date_from).days - 90 + 1
             prices = data['prices'][0:end]
@@ -34,49 +35,51 @@ def main(args):
         print('There is no prices available for that currency.')
         exit()
 
-    trend = downward_trend(prices)
+    trend = downward_trend(prices) #Get the maximum downward trend
 
     print(
         f"{args.currency.title()}'s price decreased {trend} days in a row for the selected time range.")
 
-    timestamp, max_volume = trading_volume(volumes)
-    volume_date = datetime.datetime.utcfromtimestamp(timestamp / 1000).date()
+    timestamp, max_volume = trading_volume(volumes) # Get highest trading volume
+    volume_date = datetime.datetime.utcfromtimestamp(timestamp / 1000).date() # Convert UNIX timestamp to a date
 
     print(
         f"{args.currency.title()}'s highest trading volume was {round(max_volume, 2)}€ on {volume_date}.")
 
-    profit = best_profit(prices)
+    profit = best_profit(prices) # Get most profitable days and prices
     if profit['buy'] < profit['sell']:
-        buy_date = datetime.datetime.utcfromtimestamp(profit['buy'] / 1000).date()
-        sell_date = datetime.datetime.utcfromtimestamp(profit['sell'] / 1000).date()
-        print(f"Buy {args.currency.title()} on {buy_date} and sell on {sell_date} to maximize profits.")
+        buy_date = datetime.datetime.utcfromtimestamp(
+            profit['buy'] / 1000).date()
+        sell_date = datetime.datetime.utcfromtimestamp(
+            profit['sell'] / 1000).date()
+        print(
+            f"Buy {args.currency.title()} on {buy_date} and sell on {sell_date} to maximize profits.")
     else:
-        print(f"You shouldn't buy or sell {args.currency.title()} on the selected time period.")
-
-
-
+        print(
+            f"You shouldn't buy or sell {args.currency.title()} on the selected time period.")
 
 
 def downward_trend(prices):
     decrease = []
     temp = 0
     for i, d in enumerate(prices):
-        if i > 0:
-            if d[1] < prices[i - 1][1]:
+        if i > 0: # If not first one
+            if d[1] < prices[i - 1][1]: # If price is less than the days before
                 temp += 1
             else:
                 decrease.append(temp)
                 temp = 0
 
     decrease.append(temp)
-    return max(decrease)
+    return max(decrease) #Return highest value
 
 
 def trading_volume(volumes):
-    max_volume = max(x[1] for x in volumes)
-    timestamp = [x[0] for x in volumes if x[1] == max_volume][0]
+    max_volume = max(x[1] for x in volumes) #highest volume
+    timestamp = [x[0] for x in volumes if x[1] == max_volume][0] # find the timestamp
 
     return timestamp, max_volume
+
 
 def best_profit(prices):
     profit = []
@@ -97,9 +100,13 @@ def best_profit(prices):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Dates are accepted in ISO 8601 format. (YYYY-MM-DD)")
-    parser.add_argument('date_from', type=datetime.datetime.fromisoformat, help="Start date of the time period.")
-    parser.add_argument('date_to', type=datetime.datetime.fromisoformat, help="End date of the time period.")
-    parser.add_argument('-c', '--currency', nargs="?", type=str, help="The cryptocurrency used in the calculations. Default is Bitcoin", default="bitcoin")
+    parser = argparse.ArgumentParser(
+        description="Dates are accepted in ISO 8601 format. (YYYY-MM-DD)")
+    parser.add_argument('date_from', type=datetime.datetime.fromisoformat,
+                        help="Start date of the time period.")
+    parser.add_argument('date_to', type=datetime.datetime.fromisoformat,
+                        help="End date of the time period.")
+    parser.add_argument('--currency', nargs="?", type=str,
+                        help="The cryptocurrency used in the calculations. Default is Bitcoin", default="bitcoin")
     args = parser.parse_args()
     main(args)
